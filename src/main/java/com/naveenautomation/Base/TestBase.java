b8bb3bbfedd0fe96fa8658831b8b40484bfed386
HarinderKaur91 <kaur.harinder91@gmail.com>
2023-01-24 12:56:18 -0700

    Web hook

--- src/main/java/com/naveenautomation/Base/TestBase.java
+++ src/main/java/com/naveenautomation/Base/TestBase.java
@@ -16,7 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 
-	//Testing Webhook
+	//Testing Web hook
 	public static WebDriver driver;
 	public Browsers DEFAULT_BROWSER = Browsers.EDGE;
 
